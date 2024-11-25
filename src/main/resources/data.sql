aa07ac075a5c3c8b06392412ef2010e514ff98b4
carlosjc31 <carlosjc31@hotmail.com>
2024-11-25 17:46:18 -0300

    4º criando o insert no h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO CIDADE(nome, estado, populacao, regiao) VALUES('Sorocaba', 'São Paulo', 710000, 'Sudeste')
+INSERT INTO CIDADE(nome, estado, populacao, regiao) VALUES('Itu', 'São Paulo', 220000, 'Sudeste')
+INSERT INTO CIDADE(nome, estado, populacao, regiao) VALUES('Londrina', 'Paraná', 610000, 'Sul')
