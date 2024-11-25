109c21881938ae17f2cb6f84386cfa5a1bd0f971
carlosjc31 <carlosjc31@hotmail.com>
2024-11-25 16:49:56 -0300

    criando entidades

---
+++ src/main/java/com/cidades/cidades/entities/Cidade.java
@@ -0,0 +1,76 @@
+package com.cidades.cidades.entities;
+
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cidade implements Serializable{
+    //Entidades
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String estado;
+    private int populacao;
+    private String regiao;
+
+    //Getters and Setters
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public int getPopulacao() {
+        return populacao;
+    }
+    public void setPopulacao(int populacao) {
+        this.populacao = populacao;
+    }
+    public String getRegiao() {
+        return regiao;
+    }
+    public void setRegiao(String regiao) {
+        this.regiao = regiao;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cidade other = (Cidade) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    
+}
