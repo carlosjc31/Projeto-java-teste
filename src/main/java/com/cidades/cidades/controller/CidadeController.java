b7b36f41dac4f2c9cc67525be03143f69d967c7f
carlosjc31 <carlosjc31@hotmail.com>
2024-11-26 23:20:20 -0300

    11º no controller criando um saveCidade

--- src/main/java/com/cidades/cidades/controller/CidadeController.java
+++ src/main/java/com/cidades/cidades/controller/CidadeController.java
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +37,20 @@ public class CidadeController {
     public ResponseEntity<Void> deleteCidade(@PathVariable long id){
         service.delete(id);
         return  ResponseEntity.noContent().build(); 
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> updateCidade(@PathVariable long id, 
+                                             @RequestBody Cidade cidade){
+
+        service.update(cidade, id);
+        return ResponseEntity.ok().build();  
+    }
+
+    @PostMapping
+    public ResponseEntity<Cidade> saveCidade(@RequestBody Cidade cidade){
+        Cidade newCidade =service.save(cidade);
+        return ResponseEntity.created(null).body(newCidade);
     }
     
 }
